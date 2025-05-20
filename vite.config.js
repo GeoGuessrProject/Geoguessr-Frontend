2a770fa5f4e5e77b88c4f10b95f0e0fd8f7c8257
VictorPandiweb <victorh@pandiweb.dk>
2025-05-20 15:24:27 +0200

    Set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/Geoguessr-Frontend/',
   plugins: [
     svelte(), 
     tailwindcss(),
