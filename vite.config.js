444e57be94c4ee3923590c0b763173c4648ba3a0
VictorPandiweb <victorh@pandiweb.dk>
2025-05-20 21:43:16 +0200

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/Geoguessr-Frontend/',
   plugins: [
     svelte(), 
     tailwindcss(),
