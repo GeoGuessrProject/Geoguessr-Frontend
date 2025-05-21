38bfad42269aabead323c564a4b6c4275ecacc71
VictorPandiweb <victorh@pandiweb.dk>
2025-05-21 15:38:49 +0200

    changed config

--- src/config.js
+++ src/config.js
@@ -3,11 +3,11 @@ const isDev = window.location.hostname === "localhost";
 
 export const AUTH_URL = isDev
   ? "http://localhost:8001"
-  : `https://${import.meta.env.VITE_AUTH_PROD}:8001`;
+  : `http://${import.meta.env.VITE_AUTH_PROD}:8001`;
 
 export const GAME_URL = isDev
     ? "http://localhost:8002"
-    : `https://${import.meta.env.VITE_GAME_PROD}:8002`;
+    : `http://${import.meta.env.VITE_GAME_PROD}:8002`;
 
 export const IMAGE_URL = isDev
   ? "http://localhost:8003"
